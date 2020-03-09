e1f1424ac0e5443ea1c53cbb0c16d6d782f46b2b
“Foster <fosterataylor@yahoo.com>
2020-03-09 13:28:29 -0600

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// The classic Hello World program!
+class HelloWorld {
+    public static void main(String[] args) {
+        //  Write Hello World to the terminal window
+        System.out.println("Hello foster");
+        System.out.println("Hello aurora");
+
+    }
+}
