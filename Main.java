ce46e2fe61ca5f4f1cf5cccc0f9ca54c7c788506
“Foster <fosterataylor@yahoo.com>
2020-03-09 14:50:06 -0600

    Add initial structure of user program

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    Scanner input;
+
+    Main() {
+        input = new Scanner(System.in);
+    }
+
+    public String initialMessage() {
+        System.out.println("Welcome to Foster a Post!  You can login or Sign Up!");
+        // Main main = new Main();
+        return "Foster";
+    }
+
+    public void userDashBoard(String userName, Main main) {
+        System.out.println("Welcome!!");
+    }
+    public static void main(String[] args) {
+        Main main = new Main();
+        String getUserName = main.initialMessage();
+        if (!getUserName.isBlank()) {
+            main.userDashBoard(getUserName, main);
+        }
+    }
+}
