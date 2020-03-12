05b376178fb6eb511da92a80d2c5365c9ff207bb
“Foster <fosterataylor@yahoo.com>
2020-03-12 09:25:35 -0600

    Add userData class with initial friends and updates userData

--- src/sample/UserData.java
+++ src/sample/UserData.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
 
 public class UserData {
+    // This class will hold all data related to user actions
+    ArrayList<User> userData;
+    ArrayList<String> friend1;
+    ArrayList<String> friend2;
+    ArrayList<String> update1;
+    ArrayList<String> update2;
+    ArrayList<String> update3;
+    User user1;
+    User user2;
+    User user3;
+
+    UserData() {
+        userData = new ArrayList<User>();
+
+        friend1 = new ArrayList<String>();
+        friend1.add("Dave");
+        friend1.add("Daniel");
+
+        update1 = new ArrayList<String>();
+        update1.add("Hello my friends!");
+        update1.add("Whats going on my friends!");
+        update1.add("Awfully quiet my friends!");
+
+        
+    }
+
+
+
+
+
+
     public static void main(String[] args) {
         
     }
