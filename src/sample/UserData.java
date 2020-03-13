7bb68074d687ac78177ae362c06bedd033f1afdf
“Foster <fosterataylor@yahoo.com>
2020-03-13 14:16:28 -0600

    Add intial structure to be able to add friends for a user

--- src/sample/UserData.java
+++ src/sample/UserData.java
@@ -66,6 +66,16 @@ public class UserData {
         return new ArrayList<String>(); 
     }
 
+    public ArrayList<String> addBuddies(String userName, String newFriend) {
+        User currentUser;
+        for (User user : userData) {
+            if (user.myUserName.equals(userName)) {
+                currentUser = user;
+                return currentUser.friends.add(newFriend);   
+            }
+        }
+    }
+
 
 
 
